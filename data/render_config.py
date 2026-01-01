a187da2e81429ed1b1675243b0a76bbc75b0d294
mihast <mihast2003@gmail.com>
2026-01-01 14:06:42 +0300

    fixing render config

--- data/render_config.py
+++ data/render_config.py
@@ -1,10 +1,8 @@
 
 RENDER_CONFIG = {
 
-    "pixel_ratio": 0.2,  #png image pixel/screen pixel ratio
+    "pet_size_on_screen": 8.5,  # vertical scale of first sprite on the scren (in percent)
 
     "drag_offset_x": 0,
     "drag_offset_y": -1.4,
-
 }
-
